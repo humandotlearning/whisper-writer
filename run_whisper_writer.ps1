0397262e59069aa12ced4ffad5b21f294afe30e5
humandotlearning <nithin.varghese1357@gmail.com>
2024-08-30 18:34:55 +0530

    + easy startup script

---
+++ run_whisper_writer.ps1
@@ -0,0 +1,8 @@
+# Change to the Whisper Writer directory
+Set-Location -Path "C:\Users\nithi\Documents\github\github-mine\whisper-writer"
+
+# Activate the Conda environment
+conda activate py310
+
+# Run the Python script
+python .\run.py
